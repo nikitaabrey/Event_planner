6ca13c39ae8f2d68f4dd517e479255e8d521c47f
Nate-5 <99980831+Nate-5@users.noreply.github.com>
2022-04-12 16:51:49 +0200

    Added  TableCreation and TablePopulate script

---
+++ constants/scripts/TablePopulate_script.sql
@@ -0,0 +1,18 @@
+USE EventPlannerDB;
+GO
+
+--Populate EventPlanner Tables
+--*****************************************************************************************************
+
+INSERT INTO Users (FirstName,LastName,Email)
+VALUES
+    ('Ahmed','Campbell','tellus@icloud.edu')
+	('Constance','Bartlett','maecenas@outlook.edu'),
+	('Mannix','Booker','purus@google.net'),
+	('Slade','Roberson','magnis@google.ca'),
+	('Dante','Cooper','vitae@yahoo.edu'),
+	('Rowan','Sweet','elit@google.org'),
+	('Amir','Mathews','mi@google.com'),
+	('Raymond','Yang','turpis@protonmail.ca');
+
+	
